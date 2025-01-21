42851f292e68c694cc7dd95a9df4e950b8700df8
Ryan2104 <ryansilva2125@outlook.com>
2025-01-20 22:21:21 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'GetTask_ryan',
+  webDir: 'www'
+};
+
+export default config;
